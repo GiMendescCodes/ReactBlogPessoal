0fc5c13d550ba6d7a4fa8d783cbba427735e545d
GimendescCodes <giovannamendessenai@gmail.com>
2026-07-06 17:17:50 -0300

    navbar-atualizada

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -22,75 +22,42 @@ function Navbar() {
             <nav className="w-full flex justify-center py-4 bg-[#4F46C8] text-white shadow-md">
                 <div className="container flex justify-between items-center mx-8">
 
-                    {/* Logo */}
                     <Link
                         to="/home"
-                        className="text-2xl font-bold px-2 py-1 rounded-md relative
-      hover:scale-105 hover:bg-white/10 transition-all duration-200"
+                        className="text-2xl font-bold transition-transform duration-200 hover:scale-105 relative
+      after:content-[''] after:absolute after:left-0 after:-bottom-1
+      after:h-[2px] after:w-0 after:bg-white
+      hover:after:w-full after:transition-all after:duration-300"
                     >
                         Blog Pessoal
                     </Link>
 
-                    {/* Links */}
-                    <div className="flex items-center gap-4 text-lg">
+                    <div className="flex items-center gap-6 text-lg">
 
-                        <Link
-                            to="/postagens"
-                            className="px-2 py-1 rounded-md relative
-        hover:bg-white/10 hover:scale-105 hover:text-[#E8E6FF]
-        transition-all duration-200
-        after:content-[''] after:absolute after:left-0 after:bottom-0
-        after:h-[2px] after:w-0 after:bg-white
-        hover:after:w-full after:transition-all after:duration-300"
-                        >
-                            Postagens
-                        </Link>
+                        {[
+                            { to: "/postagens", label: "Postagens" },
+                            { to: "/temas", label: "Temas" },
+                            { to: "/cadastrartema", label: "Cadastrar tema" },
+                            { to: "/perfil", label: "Perfil" },
+                        ].map((item) => (
+                            <Link
+                                key={item.to}
+                                to={item.to}
+                                className="relative transition-transform duration-200 hover:scale-105
+          after:content-[''] after:absolute after:left-0 after:-bottom-1
+          after:h-[2px] after:w-0 after:bg-white
+          hover:after:w-full after:transition-all after:duration-300"
+                            >
+                                {item.label}
+                            </Link>
+                        ))}
 
-                        <Link
-                            to="/temas"
-                            className="px-2 py-1 rounded-md relative
-        hover:bg-white/10 hover:scale-105 hover:text-[#E8E6FF]
-        transition-all duration-200
-        after:content-[''] after:absolute after:left-0 after:bottom-0
-        after:h-[2px] after:w-0 after:bg-white
-        hover:after:w-full after:transition-all after:duration-300"
-                        >
-                            Temas
-                        </Link>
-
-                        <Link
-                            to="/cadastrartema"
-                            className="px-2 py-1 rounded-md relative
-        hover:bg-white/10 hover:scale-105 hover:text-[#E8E6FF]
-        transition-all duration-200
-        after:content-[''] after:absolute after:left-0 after:bottom-0
-        after:h-[2px] after:w-0 after:bg-white
-        hover:after:w-full after:transition-all after:duration-300"
-                        >
-                            Cadastrar tema
-                        </Link>
-
-                        <Link
-                            to="/perfil"
-                            className="px-2 py-1 rounded-md relative
-        hover:bg-white/10 hover:scale-105 hover:text-[#E8E6FF]
-        transition-all duration-200
-        after:content-[''] after:absolute after:left-0 after:bottom-0
-        after:h-[2px] after:w-0 after:bg-white
-        hover:after:w-full after:transition-all after:duration-300"
-                        >
-                            Perfil
-                        </Link>
-
-                        <Link
-                            to=""
+                        <button
                             onClick={logout}
-                            className="px-2 py-1 rounded-md relative
-        hover:bg-red-500/20 hover:scale-105 hover:text-red-200
-        transition-all duration-200"
+                            className="relative transition-transform duration-200 hover:scale-105"
                         >
                             Sair
-                        </Link>
+                        </button>
 
                     </div>
                 </div>
